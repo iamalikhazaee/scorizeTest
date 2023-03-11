01fe9bfb6929c6ccdb77cfb6305f685b227a3cd9
alikhazaei <alikhazaee00@gmail.com>
2023-03-11 14:27:03 +0330

    pagination added

--- App/views.py
+++ App/views.py
@@ -2,10 +2,16 @@ from django.shortcuts import render
 
 # Create your views here.
 from rest_framework import viewsets
+from rest_framework.pagination import PageNumberPagination
 from .serializer import *
 from .models import *
 
+from rest_framework import pagination
+
+class ExamplePagination(pagination.PageNumberPagination):
+       page_size = 10
 
 class UniversityViewSet(viewsets.ModelViewSet):
     queryset = University.objects.all()
     serializer_class = UniversityListSerializer
+    pagination_class = ExamplePagination
