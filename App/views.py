132fee249ae10ba24eddd8a6c7ec24ccf62b82e3
alikhazaei <alikhazaee00@gmail.com>
2023-03-11 14:37:42 +0330

    searching university name and country and city added

--- App/views.py
+++ App/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from rest_framework import viewsets
 from rest_framework import pagination
+from rest_framework.filters import SearchFilter
 from .serializer import *
 from .models import *
 
@@ -12,6 +13,8 @@ class ExamplePagination(pagination.PageNumberPagination):
        page_size = 10
 
 class UniversityViewSet(viewsets.ModelViewSet):
+    filter_backends = [SearchFilter]
+    search_fields = ["name", "city__city", "city__country__country"]
     queryset = University.objects.all()
     serializer_class = UniversityListSerializer
     pagination_class = ExamplePagination
